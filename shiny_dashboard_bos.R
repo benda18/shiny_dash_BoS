6cde07d22c949efe57da7ae98ba878dbffd75173
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-04-01 19:34:59 -0400

    Update shiny_dashboard_bos.R

--- shiny_dashboard_bos.R
+++ shiny_dashboard_bos.R
@@ -1,14 +1,15 @@
 library(dplyr)
 library(shiny)
 
+rm(list=ls());cat('\f');gc()
+
+# background----
+current.dash <- "https://public.tableau.com/app/profile/nccehdatacenter/viz/NCBalanceofStateCoCHMISDashboard/BoS"
 
 # funs----
 
 # vars----
 the.wd <- "C:/Users/TimBender/Documents/R/ncceh/projects/shiny_dash"
-
-
-
 
 # setup----
 setwd(the.wd)
