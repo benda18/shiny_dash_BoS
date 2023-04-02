5232e54600f7289667d189d475283865c530d3c8
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-04-02 14:03:40 -0400

    Update MODULE_data.R
    updated filenames and urls

--- modules/MODULE_data.R
+++ modules/MODULE_data.R
@@ -10,13 +10,13 @@ co_reg_cw        <- read_csv("https://raw.githubusercontent.com/timbender-ncceh/
 
 # load regular csv----
 setwd(wd_csv)
-client           <- read_csv(NA)
-enrollmentcoc    <- read_csv(NA)
-enrollment       <- read_csv(NA)
-projectcoc       <- read_csv(NA)
-project          <- read_csv(NA)
-inventory        <- read_csv(NA)
-export           <- read_csv(NA)
+client           <- read_csv("Client.csv")
+enrollmentcoc    <- read_csv("EnrollmentCoC.csv")
+enrollment       <- read_csv("Enrollment.csv")
+projectcoc       <- read_csv("ProjectCoC.csv")
+project          <- read_csv("Project.csv")
+inventory        <- read_csv("Inventory.csv")
+export           <- read_csv("Export.csv")
 
 # load lookback csv----
 setwd(wd_lb)
@@ -24,6 +24,3 @@ LB_enrollmentcoc <- read_csv(NA)
 LB_enrollment    <- read_csv(NA)
 LB_projectcoc    <- read_csv(NA)
 LB_project       <- read_csv(NA)
-
-
-
