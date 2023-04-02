12d874ecb51b99b6cd76e60c3d41ac7c74183b78
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-04-02 14:24:01 -0400

    Update MODULE_data.R

--- modules/MODULE_data.R
+++ modules/MODULE_data.R
@@ -4,25 +4,40 @@ library(readr)
 # RESOURCES----
 # crosswalks: https://github.com/timbender-ncceh/PIT_HIC/tree/main/crosswalks
 # common HMIS functions: https://github.com/timbender-ncceh/PIT_HIC/blob/main/working_files/pit_survey_calculations.R
+# chronically homeless module: https://github.com/timbender-ncceh/PIT_HIC/blob/main/working_files/pit_MODULE_chronicallyhomeless.R
 
 # CAPTURE INITIAL STATE METADATA----
+
 # Write current wd so you can set it back to that once this script has run
 init.wd <- getwd()
+
 # Write current vars so you don't accidentally remove any
 init.vars <- c(ls(),"init.vars", "use.these")
+
 # load common HMIS functions----
 devtools::source_url(url = "https://raw.githubusercontent.com/timbender-ncceh/PIT_HIC/dev/working_files/pit_survey_calculations.R?raw=TRUE")
+
 # get common hmis functions names
 common.hmis.funs <- ls()[!ls() %in% init.vars]
 
 
-# Vars----
-keep.these.common.funs <- c(NA)
+# VARS----
+grep("ch", common.hmis.funs, ignore.case = T, value = T)
 
+keep.these.common.funs <- c("fun_gender", 
+                            "fun_race",
+                            "calc_age", 
+                            "hud_age_category", 
+                            "get.calc_location_county", 
+                            "get.proj_county", 
+                            "get.calc_region", 
+                            "get_coc_region")
 
 # Remove un-needed functions to save memory----
-gc()
+rm.hmis.funs <- common.hmis.funs[!common.hmis.funs %in% keep.these.common.funs]
+rm(list = rm.hmis.funs)
 
+gc()
 
 # LOAD DATA----
 # load crosswalks----
