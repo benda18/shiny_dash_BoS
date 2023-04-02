43d856595baa997a28413461f229375049caae0c
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-04-02 14:07:22 -0400

    Update MODULE_data.R

--- modules/MODULE_data.R
+++ modules/MODULE_data.R
@@ -4,6 +4,10 @@ library(readr)
 # RESOURCES----
 # crosswalks: https://github.com/timbender-ncceh/PIT_HIC/tree/main/crosswalks
 
+# Write current wd so you can set it back to that once this script has run
+init.wd <- getwd()
+
+
 # LOAD DATA----
 # load crosswalks----
 co_reg_cw        <- read_csv("https://raw.githubusercontent.com/timbender-ncceh/PIT_HIC/main/crosswalks/county_district_region_crosswalk.csv")
@@ -24,3 +28,6 @@ LB_enrollmentcoc <- read_csv("EnrollmentCoC.csv")
 LB_enrollment    <- read_csv("Enrollment.csv")
 LB_projectcoc    <- read_csv("ProjectCoC.csv")
 LB_project       <- read_csv("Project.csv")
+
+# Return WD to wd prior to script being run----
+setwd(init.wd)
