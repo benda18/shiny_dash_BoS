fa9813276e45a5567b4dd40820e5704f97a2d63a
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-04-03 09:19:28 -0400

    Update MODULE_mapping.R
    cleanup notes; turn it from a tutorial for adriana into a working product for me

--- modules/MODULE_mapping.R
+++ modules/MODULE_mapping.R
@@ -1,21 +1,4 @@
-# SAMPLE MAP TEMPLATE
-
-# This template map shows how to create a map of North Carolina's 100 counties and 
-# their Census population figures for 
-
-
-# Pre-Requisite Software----
-# 1) Download R (the programming language) from https://cran.r-project.org/bin/windows/  (click on 'install R for the first time')
-# 2) Download RStudio (the Integrated Developer Environment [IDE] from https://download1.rstudio.org/desktop/windows/RStudio-2022.07.2-576.exe)
-
-
-
-# Initiate Libraries----
-
-# Note:  The first time you use any library, you will almost certainly have to
-# install it from the 'Console' (command line) below in RStudio with the
-# following function:  install.packages("PutNameOfPackageHereInQuotes")  Note2:
-# the terms "package" and "library" are used interchangeably in R
+# MODULE_mapping
 
 library(ggplot2)
 library(tigris)
@@ -23,20 +6,15 @@ library(tidycensus)
 library(dplyr)
 library(glue)
 
-# Following is a quick line of code I always include at the beginning of every
-# script that clears out old variables from the global environment, clears
-# memory and clears the console screen. It's a good way to just start from
-# scratch so you know that nothing from your last script accidentally gets
-# carried over and used without you knowing about it.
 rm(list = ls());gc();cat('\f')  
 
 # Set Variables----
-var_state        <- "North Carolina"  # you can change this to any US state (and possibly territory)
+var_state        <- "North Carolina"  
 demographic.info <- "population"      # you can also change this to 'housing'
 var_year         <- 2020              # year of census data to pull.  i think 2019 is most recent available due to pandemic
 
 # Download All US State Boundary Geographies----
-boundary_all.states <- tigris::states(cb = T,  # TRUE = low_res, FALSE = high_res 
+boundary_all.states <- tigris::states(cb   = T,  # TRUE = low_res, FALSE = high_res 
                                       year = var_year)
 
 # this line of code filters down to the state you set in 'var_state' from the
@@ -50,30 +28,14 @@ ggplot() +
        subtitle = "Below we'll talk about changing the map theme adding more features",
        caption = "There's so much more we can do though")
 
-# you san set any map to memory as a variable for future use as follows.  This
-# is useful when you need to make multiple maps of the same area: you can create
-# a base map with all of the shared features like geographic boundaries, major
-# roadways, etc., and then later on add new features onto that map without
-# having to re-writing the same code to produce another basemap.
+# set map to var plot_basemap
 plot_basemap <- ggplot() + 
   geom_sf(data = boundary_var.state)
 
-# to plot your basemap, simply run it alone: 
+# print to plot map
 plot_basemap
 
-# now would be a good time to make some tweaks to the map theme.  we're going to
-# use the theme() function to remove the longitude and latitude coordinates from
-# this map since there's nothing that we'll be adding to this map that the
-# viewer will need to know that level of geographic precison to comprehend, plus
-# it's ugly.  Any of the values you see in the code below in green (character
-# values, need to be enclosed with quotation marks) or in red (numbers) can be
-# modified to reflect your preferences
-
-# if you need help understanding how something in R works, run it in the console
-# below with a question mark appended before it.  that will bring up the help
-# file to the right ---->
-?theme()  # for example
-
+# theme updates----
 plot_basemap <- plot_basemap +
   theme(axis.text         = element_blank(),                  # removes the longitude/latitude labels
         axis.ticks        = element_blank(),                  # removes the '-' (tick) marks from the axis
@@ -93,16 +55,10 @@ plot_basemap <- plot_basemap +
         legend.background = element_rect(fill = "light green",# color name, code, etc 
                                          color = "green")) +  # color name, code, etc 
   labs(title    = "[Title - Update for each final map]",      # Set the Map title here 
-       subtitle = glue("{var_state}, USA, {var_year}"),       # -No need to update; I've used the 
-       # 'glue()' function along with 'var_state' 
-       # and 'var_year' variables to auto-generate 
-       # this title so that when these variables 
-       # are changed, this text will update automatically
+       subtitle = glue("{var_state}, USA, {var_year}"),       # generated from vars
        caption  = "Source: US Census Bureau, Accessed 2023")  # Set the Map caption here 
 
-# notice that we saved the basemap with the new theme and 'labs' elements
-# overwriting 'plot_basemap'.  now when we plot the basemap, we have a more
-# complete and finished-looking map
+# print to view map
 plot_basemap
 
 # HOW TO ADD ADDITIONAL GEOGRAPHIC FEATURES TO YOUR MAP----
@@ -112,9 +68,9 @@ plot_basemap
 # library in R.  The basemap produced so far has used state boundaries pulled
 # from tigris().  But let's explore some of the other things we can add to our
 # basemap to help the map's audience better orient themselves to their location
-# in the state by lownloading major road and water features.
+# in the state by downloading major road and water features.
 
-
+# add counties----
 county_names <- tigris::counties(state = var_state, cb = T, year = var_year)$NAME
 gc()
 
