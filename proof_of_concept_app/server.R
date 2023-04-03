d5df2f77a45e989051436d0965bef1e8fe50b6a8
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-04-03 14:17:24 -0400

    Update server.R
    updated with my proof of concept; works

--- proof_of_concept_app/server.R
+++ proof_of_concept_app/server.R
@@ -1,30 +1,35 @@
+library(dplyr)
+library(readr)
 library(shiny)
+library(data.table)
 library(ggplot2)
 
 function(input, output) {
-
-  dataset <- reactive({
-    diamonds[sample(nrow(diamonds), input$sampleSize),]
+  # load data
+  pretend.df <- read_csv("https://raw.githubusercontent.com/timbender-ncceh/shiny_dash_BoS/main/proof_of_concept_app/summary_agegroups_by_region.csv")
+  devtools::source_url(url = "https://raw.githubusercontent.com/timbender-ncceh/shiny_dash_BoS/main/modules/MODULE_mapping.R?raw=TRUE")
+  
+  # other stuff
+  output$basemap01 <- renderPlot({
+    basemap+
+      geom_sf(data = bos_regions[bos_regions$Region %in% input$checkGroup01,], # filter regions here 
+              color = "white",
+              aes(fill = Region_f))+
+      geom_sf(data = nc_bound, 
+              color = "grey", 
+              fill = NA) +
+      geom_sf(data = bos_counties[bos_counties$Region %in% input$checkGroup01,],  # filter regions here
+              fill = NA, color = "black") +
+      coord_sf(xlim = range(bbox.nc[c("xmin", "xmax")]), 
+               ylim = range(bbox.nc[c("ymin", "ymax")]))+
+      scale_fill_discrete(name = "BoS Region")
   })
-
-  output$plot <- renderPlot({
-
-    p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
-
-    if (input$color != 'None')
-      p <- p + aes_string(color=input$color)
-
-    facets <- paste(input$facet_row, '~', input$facet_col)
-    if (facets != '. ~ .')
-      p <- p + facet_grid(facets)
-
-    if (input$jitter)
-      p <- p + geom_jitter()
-    if (input$smooth)
-      p <- p + geom_smooth()
-
-    print(p)
-
-  }, height=700)
-
+  output$table01 <- renderTable({
+    pretend.df[pretend.df$Region %in% input$checkGroup01,]
+  })
+  # You can access the values of the widget (as a vector)
+  # with input$checkGroup01, e.g.
+  output$value01 <- renderPrint({ 
+    input$checkGroup01 
+  })
 }
