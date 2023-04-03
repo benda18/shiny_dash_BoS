88baff471b109fca280bf59bffed378a442245b8
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-04-03 13:55:03 -0400

    Update mvp.R
    cleanup; not tested.

--- mvp.R
+++ mvp.R
@@ -7,27 +7,28 @@ library(data.table)
 
 rm(list=ls()[!ls() %in% "test.data"]);cat('\f');gc()
 # Funs----
-hud_age_category <- function(age_yrs, 
-                             breaks_upper = c(17,24,34,44,54,64,126)){
-  require(dplyr)
-  require(data.table)
-  
-  if(is.na(age_yrs) | age_yrs > max(breaks_upper)){
-    out <- NA
-  }else{
-    bac <- data.frame(lower = 0, upper = breaks_upper[1])
-    for(i in 2:length(breaks_upper)){
-      bac <- rbind(bac, 
-                   data.frame(lower = breaks_upper[i-1]+1, 
-                              upper = breaks_upper[i]))
-    }
-    bac <- mutate(bac, name = paste(lower, upper, sep = "-"))
-    bac$in_cat <- between(x = rep(age_yrs, nrow(bac)), 
-                          lower = bac$lower, upper = bac$upper)
-    out <- bac[bac$in_cat,]$name
-  }
-  return(out)
-}
+
+# hud_age_category <- function(age_yrs, 
+#                              breaks_upper = c(17,24,34,44,54,64,126)){
+#   require(dplyr)
+#   require(data.table)
+#   
+#   if(is.na(age_yrs) | age_yrs > max(breaks_upper)){
+#     out <- NA
+#   }else{
+#     bac <- data.frame(lower = 0, upper = breaks_upper[1])
+#     for(i in 2:length(breaks_upper)){
+#       bac <- rbind(bac, 
+#                    data.frame(lower = breaks_upper[i-1]+1, 
+#                               upper = breaks_upper[i]))
+#     }
+#     bac <- mutate(bac, name = paste(lower, upper, sep = "-"))
+#     bac$in_cat <- between(x = rep(age_yrs, nrow(bac)), 
+#                           lower = bac$lower, upper = bac$upper)
+#     out <- bac[bac$in_cat,]$name
+#   }
+#   return(out)
+# }
 
 # vars----
 wd_PROJ  <- "C:/Users/TimBender/Documents/R/ncceh/projects/shiny_dash"
@@ -62,66 +63,67 @@ devtools::source_url(url = "https://raw.githubusercontent.com/timbender-ncceh/sh
 
 # pretend dataset
 
-gen_fakeid <- function(gen_n = 1){
-  require(dplyr)
-  out <- replicate(n = gen_n, 
-                   paste(c(sample(LETTERS,1), 
-                           sample(1:9, size = 1),
-                           sample(0:9, size = 8, replace = T)), 
-                         sep = "", collapse = ""))
-  return(out)
-}
+# gen_fakeid <- function(gen_n = 1){
+#   require(dplyr)
+#   out <- replicate(n = gen_n, 
+#                    paste(c(sample(LETTERS,1), 
+#                            sample(1:9, size = 1),
+#                            sample(0:9, size = 8, replace = T)), 
+#                          sep = "", collapse = ""))
+#   return(out)
+# }
+# 
+# set.seed(as.numeric(ymd(20230403)))
+# pretend.df <- data.frame(pid = gen_fakeid(gen_n = 1000), 
+#                          age = sample(12:48, 
+#                                       size = 1000, 
+#                                       replace = T), 
+#                          hud_age_cat = NA,
+#                          race = sample(test.data$calc_race, 
+#                                        size = 1000, replace = T),
+#                          gender = sample(test.data$calc_gender, 
+#                                          size = 1000, replace = T),
+#                          County = sample(bos_counties$NAME, 
+#                                          size = 1000, 
+#                                          replace = T)) %>%
+#   as_tibble() %>%
+#   left_join(., 
+#             bos_cos_regions)
+# 
+# pretend.df$hud_age_cat <- unlist(lapply(X = pretend.df$age, 
+#                                         FUN = hud_age_category))
+# 
+# pretend.df <- pretend.df %>%
+#   group_by(Region, hud_age_cat) %>%
+#   summarise(n_pid = n_distinct(pid)) 
+# 
+# pretend.df <- pretend.df %>%
+#   as.data.table() %>%
+#   dcast(., 
+#         Region ~ hud_age_cat, fill = 0) %>%
+#   .[order(c(1, 10:13, 2:9)),] %>%
+#   as.data.frame() %>%
+#   as_tibble()
 
-set.seed(as.numeric(ymd(20230403)))
-pretend.df <- data.frame(pid = gen_fakeid(gen_n = 1000), 
-                         age = sample(12:48, 
-                                      size = 1000, 
-                                      replace = T), 
-                         hud_age_cat = NA,
-                         race = sample(test.data$calc_race, 
-                                       size = 1000, replace = T),
-                         gender = sample(test.data$calc_gender, 
-                                         size = 1000, replace = T),
-                         County = sample(bos_counties$NAME, 
-                                         size = 1000, 
-                                         replace = T)) %>%
-  as_tibble() %>%
-  left_join(., 
-            bos_cos_regions)
-
-pretend.df$hud_age_cat <- unlist(lapply(X = pretend.df$age, 
-                                        FUN = hud_age_category))
-
-pretend.df <- pretend.df %>%
-  group_by(Region, hud_age_cat) %>%
-  summarise(n_pid = n_distinct(pid)) 
-
-pretend.df <- pretend.df %>%
-  as.data.table() %>%
-  dcast(., 
-        Region ~ hud_age_cat, fill = 0) %>%
-  .[order(c(1, 10:13, 2:9)),] %>%
-  as.data.frame() %>%
-  as_tibble()
+pretend.df <- read_csv("https://raw.githubusercontent.com/timbender-ncceh/shiny_dash_BoS/main/proof_of_concept_app/summary_agegroups_by_region.csv")
 
 # SHINY APP---
-# Goals----
-sa.goals <- list(done_1 = c("hello world example"), 
-                 done_2 = c("ggplot basemap", 
-                            "show a table", 
-                            "not interactive", 
-                            "not filterable"), 
-                 step_3 = c("example slider - region; not working"), 
-                 step_4 = c("deploy to web server"),
-                 step_5 = c("link slider region to table", 
-                            "link slider region to map"), 
-                 step_6 = c("slider-region functions"),
-                 step_7 = c("nav bar"),
-                 step_8 = c("replace test.data with real_data"),
-                 step_9 = c("make entire dash functional"),
-                 step_N = c("formatting", "aesthetics"), 
-                 `step_N+1` = c("full deployment"))
-sa.goals
+# # Goals----
+# sa.goals <- list(done_1 = c("hello world example"), 
+#                  done_2 = c("ggplot basemap", 
+#                             "show a table", 
+#                             "not interactive", 
+#                             "not filterable"), 
+#                  done_3 = c("example checkbox - region"), 
+#                  done_4 = c("link slider region to table", 
+#                             "link slider region to map"),
+#                  done_5 = c("slider-region functions"),
+#                  step_6 = c("deploy to web server"),
+#                  step_7 = c("nav bar"),
+#                  step_8 = c("replace test.data with real_data"),
+#                  step_9 = c("make entire dash functional"),
+#                  step_N = c("formatting", "aesthetics"), 
+#                  `step_N+1` = c("full deployment"))
 
 # myMVP---
 # ui----
